3ab5a787e2faf9288f3640b98b6b44e0c81dec11
Kerem Celik <31195@isw.info>
2022-10-22 19:54:10 +0200

    Main
    Added main code to open up a window

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+from cProfile import run
+from turtle import screensize
+from webbrowser import BackgroundBrowser
+import pygame
 
+background_colour = (234, 212, 252)
+screen = pygame.display.set_mode((300, 300))
+
+pygame.display.set_caption('Blyat')
+screen.fill(background_colour)
+pygame.display.flip()
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
